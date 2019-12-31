37b9cd3c12a0d53da65f2fa300eeac33808db9b2
Miguel Ramires Agustin <miguel.r.agustin@gmail.com>
2019-12-31 09:55:08 -0600

    cambios

--- app/src/main/java/com/example/congnitusfirma/model/ResponseProductos.kt
+++ app/src/main/java/com/example/congnitusfirma/model/ResponseProductos.kt
@@ -1,10 +1,7 @@
 package com.example.congnitusfirma.model
 
-import android.content.ClipData
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
-import org.json.JSONArray
-import org.json.JSONObject
 
 class ResponseProductos(@SerializedName("productos")@Expose var product:List<ProductosResponse>
     )
